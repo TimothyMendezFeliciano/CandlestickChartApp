410255f21a6dfcde622334ead289be1f0f798b04
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2021-06-18 09:36:07 -0400

    On Angular 11

--- src/app/graph/rsi-chart/rsi-chart.component.spec.ts
+++ src/app/graph/rsi-chart/rsi-chart.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { RsiChartComponent } from './rsi-chart.component';
 
@@ -6,7 +6,7 @@ describe('RsiChartComponent', () => {
   let component: RsiChartComponent;
   let fixture: ComponentFixture<RsiChartComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ RsiChartComponent ]
     })
